e0f401da15c764944375a2df1130708b671b93d1
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-09-09 21:05:28 +0800

    Pages Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./assets/Pages/Login";
+import Dashboard from "./assets/Pages/Dashboard";
+import Inventory from "./assets/Pages/Inventory";
+import Order from "./assets/pages/Order";
+import Settings from "./assets/pages/Settings";
+import Analytics from "./assets/pages/Analytics";
+import Invoice from "./assets/pages/Invoice";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path={"/dashboard"} element={<Dashboard />} />{" "}
+        <Route path={"/inventory"} element={<Inventory />} />
+        <Route path={"/order"} element={<Order />} />
+        <Route path={"/settings"} element={<Settings />} />
+        <Route path={"/analytics"} element={<Analytics />} />
+        <Route path={"/invoice"} element={<Invoice />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/pages/Analytics.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Analytics() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Perform login logic here (e.g., authentication)
+    // On successful login, navigate to the dashboard
+    navigate("/dashboard");
+  };
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <h1>Analytics Page</h1>
+      <button onClick={handleLogin}>Dashboard</button>
+    </div>
+  );
+}
+
+export default Analytics;
