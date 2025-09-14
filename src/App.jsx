b680eea6c3af618616d9d95dece582848fcc0e3b
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-09-14 18:22:32 +0800

    added new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,66 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Login from "./assets/Pages/Login";
-import Dashboard from "./assets/Pages/Dashboard";
-import Inventory from "./assets/Pages/Inventory";
-import Order from "./assets/pages/Order";
-import Settings from "./assets/pages/Settings";
-import Analytics from "./assets/pages/Analytics";
-import Invoice from "./assets/pages/Invoice";
-import Users from "./Users";
+import Login from "./assets/pages/auth/Login";
+import PasswordResetRequest from "./assets/pages/auth/PasswordResetRequest";
+import PasswordResetNew from "./assets/pages/auth/PasswordResetNew";
+import Dashboard from "./assets/pages/dashboard/Dashboard";
+import Orders from "./assets/pages/orders/Order";
+import OrderDetails from "./assets/pages/orders/OrderDetails";
+import CustomerOrderHistory from "./assets/pages/orders/CustomerOrderHistory";
+import AddOrderShipping from "./assets/pages/add-order/AddOrderShipping";
+import AddOrderPayment from "./assets/pages/add-order/AddOrderPayment";
+import AddOrderInvoice from "./assets/pages/add-order/AddOrderInvoice";
+import CollectionOverview from "./assets/pages/collections/CollectionOverview";
+import NewCollection from "./assets/pages/collections/NewCollection";
+import EditCollection from "./assets/pages/collections/EditCollection";
+import Inventory from "./assets/pages/item/Inventory";
+import NewItem from "./assets/pages/item/NewItem";
+import EditItem from "./assets/pages/item/EditItem";
+import ItemDetails from "./assets/pages/item/ItemDetails";
+import Invoices from "./assets/pages/invoices/Invoice";
+import InvoiceDetails from "./assets/pages/invoices/InvoiceDetails";
+import Settings from "./assets/pages/account/Settings";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Users />} /> */}
+        {/* Authentication */}
         <Route path="/" element={<Login />} />
-        <Route path={"/dashboard"} element={<Dashboard />} />{" "}
-        <Route path={"/inventory"} element={<Inventory />} />
-        <Route path={"/order"} element={<Order />} />
-        <Route path={"/settings"} element={<Settings />} />
-        <Route path={"/analytics"} element={<Analytics />} />
-        <Route path={"/invoice"} element={<Invoice />} />
+        <Route path="/password-reset" element={<PasswordResetRequest />} />
+        <Route path="/password-reset/new" element={<PasswordResetNew />} />
+
+        {/* Main */}
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Orders */}
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/orders/:id" element={<OrderDetails />} />
+        <Route path="/customer-orders" element={<CustomerOrderHistory />} />
+
+        {/* Add Order */}
+        <Route path="/add-order/shipping" element={<AddOrderShipping />} />
+        <Route path="/add-order/payment" element={<AddOrderPayment />} />
+        <Route path="/add-order/invoice" element={<AddOrderInvoice />} />
+
+        {/* Collections */}
+        <Route path="/collections" element={<CollectionOverview />} />
+        <Route path="/collections/new" element={<NewCollection />} />
+        <Route path="/collections/:id/edit" element={<EditCollection />} />
+
+        {/* Inventory */}
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/inventory/new" element={<NewItem />} />
+        <Route path="/inventory/:id/edit" element={<EditItem />} />
+        <Route path="/inventory/:id/details" element={<ItemDetails />} />
+
+        {/* Invoices */}
+        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/invoices/:id" element={<InvoiceDetails />} />
+
+        {/* Account */}
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   );
---
+++ src/assets/pages/collections/EditCollection.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function EditCollection() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/dashboard");
+  };
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <h1>EditCollection Page</h1>
+      <button onClick={handleLogin}>Dashboard</button>
+    </div>
+  );
+}
+
+export default EditCollection;
