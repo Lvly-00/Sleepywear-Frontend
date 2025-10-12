f8562015318890ff93f8339b92a01c858c3b0d90
Lovely <128210631+Lvly-00@users.noreply.github.com>
2025-10-12 16:11:27 +0800

    updated url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// Use Vite env variable or fallback to local / deployed backend
+// Use Vite env variable
 const API_BASE_URL =
-    import.meta.env.VITE_API_URL || "http://localhost:8000";
+    import.meta.env.VITE_API_URL;
 
 const api = axios.create({
     baseURL: API_BASE_URL,
@@ -15,7 +15,6 @@ const api = axios.create({
 // Automatically get CSRF cookie before any authenticated request
 api.interceptors.request.use(async(config) => {
     if (!window.csrfLoaded) {
-        // Use BASE URL dynamically (works for local & deployed)
         await axios.get(`${API_BASE_URL}/sanctum/csrf-cookie`, {
             withCredentials: true,
         });
