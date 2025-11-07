468cf7034b52564f1197dab7ed3a08ab6b4006c0
Lovely <128210631+Lvly-00@users.noreply.github.com>
2025-11-08 05:18:37 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
-// https://vite.dev/config/
+import { fileURLToPath, URL } from 'node:url';
 export default defineConfig(({ mode }) => {
-    // Load environment variables from .env files
+    // Load environment variables
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
@@ -14,16 +13,19 @@ export default defineConfig(({ mode }) => {
         },
         plugins: [
             react(),
-            tailwindcss(), // Add Tailwind plugin here
+            tailwindcss(),
         ],
         base: '/',
         resolve: {
             alias: {
-                '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
                 '@': fileURLToPath(new URL('./src',
                     import.meta.url)),
-
+                '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
             },
+        },
+        build: {
+            outDir: 'dist',
+            sourcemap: false, // optional: turn off source maps for production
         },
     };
 });
