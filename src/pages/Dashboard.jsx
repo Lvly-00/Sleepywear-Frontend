d1c84e15de08c7893a1d8a4aaa12caaf19e87659
Lovely <128210631+Lvly-00@users.noreply.github.com>
2025-12-07 18:27:53 +0800

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -216,7 +216,7 @@ function Dashboard() {
             <ResponsiveContainer width="100%" height={320}>
               <LineChart
                 data={dailySales}
-=                margin={{ top: 20, right: 30, left: 10, bottom: 50 }}
+                margin={{ top: 20, right: 30, left: 10, bottom: 50 }}
               >
                 <XAxis
                   dataKey="date"
